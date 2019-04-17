941877cf9ac9a6399c04be91e3a5d688c1a3a52d
TitiGit <quatrieme@msn.com>
2019-04-17 20:34:06 +0200

    modif commentaire

--- src/Main.java
+++ src/Main.java
@@ -27,10 +27,10 @@ public class Main {
             // Vaut -1 quand c'est fini
             int n = 0;
 
-            // Tant que l'affectation dans la variable est possible, on boucle
-            // Lorsque la lecture du fichier est terminée l'affectation n'est
-            // plus possible !
-            // On sort donc de la boucle
+            /* Tant que l'affectation dans la variable est possible, on boucle
+            Lorsque la lecture du fichier est terminée l'affectation n'est
+             plus possible !
+            On sort donc de la boucle*/
             while (fis.read(buf) >= 0) {
                 // On écrit dans notre deuxième fichier avec l'objet adéquat
                 fos.write(buf);
@@ -40,23 +40,23 @@ public class Main {
                     System.out.print("\t" + bit + "(" + (char) bit + ")");
                 }
                 System.out.println();
-                //Nous réinitialisons le buffer à vide
-                //au cas où les derniers byte lus ne soient pas un multiple de 8
-                //Ceci permet d'avoir un buffer vierge à chaque lecture et ne pas avoir de doublon en fin de fichier
+                /*Nous réinitialisons le buffer à vide
+                au cas où les derniers byte lus ne soient pas un multiple de 8
+                Ceci permet d'avoir un buffer vierge à chaque lecture et ne pas avoir de doublon en fin de fichier*/
                 buf = new byte[8];
 
             }
             System.out.println("Copie terminée !");
 
         } catch (IOException e) {
-            // Cette exception est levée si l'objet FileInputStream ne trouve
-            // aucun fichier
+            /* Cette exception est levée si l'objet FileInputStream ne trouve
+            aucun fichier*/
             e.printStackTrace();
         }// Celle-ci se produit lors d'une erreur d'écriture ou de lecture
         finally {
-            // On ferme nos flux de données dans un bloc finally pour s'assurer
-            // que ces instructions seront exécutées dans tous les cas même si
-            // une exception est levée !
+            /* On ferme nos flux de données dans un bloc finally pour s'assurer
+            que ces instructions seront exécutées dans tous les cas même si
+            une exception est levée !*/
             try {
                 if (fis != null)
                     fis.close();
